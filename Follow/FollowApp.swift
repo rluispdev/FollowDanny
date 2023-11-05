3e69f915df1e9ae33b1624396177f08fb7ca53b3
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-11-05 12:30:43 -0300

    Commit inicial

---
+++ Follow/FollowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowApp.swift
+//  Follow
+//
+//  Created by Rafael Gonzaga on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FollowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ProfileView()
+        }
+    }
+}
---
+++ Follow/ProfileView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  Follow
+//
+//  Created by Rafael Gonzaga on 04/11/23.
+//
+
+import SwiftUI
+
+struct User {
+    
+    var picture: String
+    var name: String
+    var nick: String
+    var followers: Double
+    
+}
+
+struct ProfileView: View {
+    
+ var user = User(picture: "danny", name: "Danny Phantom", nick: "@dannyphantom", followers: 22_643)
+    
+    var body: some View {
+        VStack {
+            ///Perfil
+            Image("danny")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 250, height: 250)
+                .padding(.bottom, 50)
+                
+            Text("Danny Phantom")
+                .font(.system(size: 40, weight: .bold))
+            
+            Text("@dannyphantom")
+                .font(.system(size:23, weight: .regular))
+                .foregroundStyle(.gray)
+            
+            Text("22.643K")
+                .font(.system(size: 70, weight: .light))
+                .padding(40)
+        }
+        .padding()
+        
+        VStack {
+            ///Seguir
+            Button {} label: {
+                Label ("follow", systemImage: "person.fill.badge.plus" )
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            
+            ///Enviar mensagem
+            Button {} label: {
+                Label ("enviar mensagem", systemImage: "paperplane.fill" )
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            .disabled(true)
+        }
+        .padding(20)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
