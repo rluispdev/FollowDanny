aed310eaad4b6a6b50c75aa4ded92b31be2f6e58
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-11-05 14:48:04 -0300

    Criar um metodo que formata um int

--- Follow/ProfileViewModel.swift
+++ Follow/ProfileViewModel.swift
@@ -10,11 +10,24 @@ import Foundation
 struct ProfileViewModel {
     
  var isFollowing = false
+ var userFollowers = String()
     
     var user = User(picture: "danny",
                     name: "Danny Phantom",
                     nick: "@dannyphantom",
                     followers: 22_643)
+    
+    func customizeNumber(value: Double) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.locale =  Locale(identifier: "pt_BR")
+        let shorten = formatter.string(for: value) ?? "0"
+        return "\(shorten) K"
+    }
+    
+    
+    
+    
    
     mutating func followToogle(){
         self.isFollowing.toggle()
